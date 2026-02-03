777d4f1e0ad4db8d3948517aea7d88460ad477a4
Davve420 <davidbrolin04@gmail.com>
2026-02-03 14:36:35 +0100

    fixat filter & sort, börjat med 3d styling för böckerna.

--- src/main.js
+++ src/main.js
@@ -1,49 +1,60 @@
 import { postInput, books } from "./modules/firebase.js";
-import { sortByAuthorAsc, sortByTitleAsc, sortByTitleDesc, sortByAuthorDesc, filterByFavorite, sortByFirstLast, sortByLastFirst } from "./modules/sort&filter.js";
+import { sortByAuthorAsc, sortByTitleAsc, sortByTitleDesc, sortByAuthorDesc, filterByFavorite, sortByLastFirst } from "./modules/sort&filter.js";
 
 const checkbox = document.querySelector('#favorites');
 const sortSelect = document.querySelector('select');
 
-let currentSort = 'title-asc'; 
+let currentSort = 'title-asc';
 
-function renderBooks() {
+function renderBooks(books) {
     const wrapper = document.querySelector('#books');
     wrapper.innerHTML = '';
-    
-    let result = books;
-    if(checkbox.checked) {
-        result = filterByFavorite(result); 
-    }
-    
-    if(currentSort === 'title-asc') {
-        result = sortByTitleAsc(result);
-    } else if(currentSort === 'title-desc') {
-        result = sortByTitleDesc(result);
-    } else if(currentSort === 'author-asc') {
-        result = sortByAuthorAsc(result);
-    }else if(currentSort === 'author-desc') {
-        result = sortByAuthorDesc(result);
-    } else if(currentSort === 'first-last') {
-        result = sortByFirstLast(result);
-    } else if(currentSort === 'last-first') {
-        result = sortByLastFirst(result);
-    }
-    
-
-    result.forEach(book => book.renderBook(wrapper));
+    books.forEach(book => book.renderBook(wrapper));
 }
 
 checkbox.addEventListener('change', () => {
-    renderBooks();
+    let result = [...books];
+    if (checkbox.checked) {
+        result = filterByFavorite(books);
+    }
+    currentSort = document.querySelector('select').value;
+    if (currentSort === 'title-asc') {
+        result = sortByTitleAsc(result);
+    } else if (currentSort === 'title-desc') {
+        result = sortByTitleDesc(result);
+    } else if (currentSort === 'author-asc') {
+        result = sortByAuthorAsc(result);
+    } else if (currentSort === 'author-desc') {
+        result = sortByAuthorDesc(result);
+    } else if (currentSort === 'last-first') {
+        result = sortByLastFirst(result);
+    }
+    renderBooks(result);
 });
 
 sortSelect.addEventListener('change', (event) => {
-    currentSort = event.target.value; 
-    renderBooks(); 
+    let result = [...books];
+    if (checkbox.checked) {
+        result = filterByFavorite(books);
+    }
+    currentSort = event.target.value;
+    if (currentSort === 'title-asc') {
+        result = sortByTitleAsc(result);
+    } else if (currentSort === 'title-desc') {
+        result = sortByTitleDesc(result);
+    } else if (currentSort === 'author-asc') {
+        result = sortByAuthorAsc(result);
+    } else if (currentSort === 'author-desc') {
+        result = sortByAuthorDesc(result);
+    } else if (currentSort === 'last-first') {
+        result = sortByLastFirst(result);
+    }
+
+    renderBooks(result);
 });
 
 const myForm = document.querySelector('#inputform');
-myForm.addEventListener('submit', event =>{
+myForm.addEventListener('submit', event => {
     event.preventDefault()
     postInput(myForm);
     myForm.reset();
--- src/modules/sort&filter.js
+++ src/modules/sort&filter.js
@@ -10,9 +10,6 @@ Sortering och filtrering ska fungera tillsammans. Väljs en ny filtrering ska sa
 //funkade inte att skriva '? favorite: true' i onValue referensen
 //kolla på claras github FP1 för exempel kod på hur du ska tänka gällande checkboxens filtrering.
 
-
-
-
 const isFavorite = (book) => book.favorite === true;
 
 export function filterByFavorite(booksArray){
@@ -21,31 +18,30 @@ export function filterByFavorite(booksArray){
 
 //Sortera efter titel A-Ö
 export const sortByTitleAsc = (booksArray) => {
-    return booksArray.sort((a, b) => a.title.localeCompare(b.title));
+    const cloneArr = [...booksArray];
+    return cloneArr.sort((a, b) => a.title.localeCompare(b.title));
 };
 
 //Sortera efter titel Ö-A  
 export const sortByTitleDesc = (booksArray) => {
-    return booksArray.sort((a, b) => b.title.localeCompare(a.title));
+    const cloneArr = [...booksArray];
+    return cloneArr.sort((a, b) => b.title.localeCompare(a.title));
 };
 
 export const sortByAuthorDesc = (booksArray) => {
-    return booksArray.sort((a, b) => b.author.localeCompare(a.author));
+    const cloneArr = [...booksArray];
+    return cloneArr.sort((a, b) => b.author.localeCompare(a.author));
 };
 
 export const sortByAuthorAsc = (booksArray) => {
-    return booksArray.sort((a, b) => a.author.localeCompare(b.author));
-};
-
-//kolla igenom dessa så du lär dig. den gör inte riktigt exakt det vi vill kolla på sidan praktiskt.
-// First-last: behåll original ordning från arrayen
-export const sortByFirstLast = (booksArray) => {
-    return booksArray; // Returnera som den är
+    const cloneArr = [...booksArray];
+    return cloneArr.sort((a, b) => a.author.localeCompare(b.author));
 };
 
 // Last-first: reversera ordningen
 export const sortByLastFirst = (booksArray) => {
-    return booksArray.reverse();
+    const cloneArr = [...booksArray];
+    return cloneArr.reverse();
 };
 
 
